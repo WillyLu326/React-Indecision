8257aca2da5d0faa57bd7ef250dd6baaae1be1f4
WillyLu326 <luzh0326@gmail.com>
2017-10-23 21:52:42 -0400

    create hello world react.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,3 +1,12 @@
 console.log('react.js app is running');
 
 // JSX Javascript XML
+
+var template = React.createElement(
+    'h1',
+    { id: 'someid' },
+    'Hello React.js'
+);
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
